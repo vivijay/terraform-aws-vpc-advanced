9fe999d51782b4f1d93259f17151bc2cf59ca2b6
Vinit Vijay <vinit@Vinits-MacBook-Air.local>
2023-09-16 19:37:17 +0530

    new files

---
+++ variables.tf
@@ -0,0 +1,67 @@
+variable "cidr_block" {
+  
+}
+
+variable "enable_dns_support" {
+   default = "true"
+}
+variable "enable_dns_hostnames" {
+  default = "true"
+}
+
+variable "common_tags" {
+  
+}
+
+variable "vpc_tags" {
+  default = {}
+}
+
+variable "project_name" {
+  
+}
+
+variable "public_subnet_cidr" {
+    type = list
+    validation {
+    condition     = length(var.public_subnet_cidr) == 2
+    error_message = "Please provide 2 public subnet CIDR"
+  }
+}
+
+variable "private_subnet_cidr" {
+    type = list
+    validation {
+    condition     = length(var.private_subnet_cidr) == 2
+    error_message = "Please provide 2 private subnet CIDR"
+  }
+}
+
+variable "database_subnet_cidr" {
+    type = list
+    validation {
+    condition     = length(var.database_subnet_cidr) == 2
+    error_message = "Please provide 2 database subnet CIDR"
+  }
+}
+
+variable "nat_gateway_tags" {
+    default = {}
+  
+}
+
+variable "private_route_table_tags" {
+  default = {}
+}
+variable "public_route_table_tags" {
+  default = {}
+}
+variable "database_route_table_tags" {
+  default = {}
+}
+
+variable "db_subnet_group" {
+    default = {}
+  
+}
+
